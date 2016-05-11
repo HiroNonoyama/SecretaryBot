4baa8a28ad8670f7c04b6b3b9c69b148b87818ec
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-05-11 11:31:14 +0900

    [Add] todo

--- views.py
+++ views.py
@@ -1,17 +1,23 @@
 from flask import Blueprint, Response, request
 from flask.views import MethodView
 from models import Todo
-from tasks.todo import TodoTask
+from tasks.record_handler import TodoTask
 
 todos = Blueprint('todos', __name__)
 
 
-class ListView(MethodView):
+class BotView(MethodView):
     def get(self):
         todo = Todo.objects.first()
         task = TodoTask()
         task.create(obj_cls=Todo, title='this is mongo')
         return todo.title
 
-todos.add_url_rule('/', view_func=ListView.as_view('list'))
+    def post(self):
+        req = request.json
+        # print req['result'][0]
+
+        return Response(status=200)
+
+todos.add_url_rule('/', view_func=BotView.as_view('list'))
 
